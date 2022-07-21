2eba11717102c3bb4d1a03f3533e56677c681f2d
benjaminhirsch <benjaminhirsch@users.noreply.github.com>
2022-07-21 19:59:44 +0000

    deploy: 99d01bed59ae13f4f82b595641753cb39809d5f1

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  content: [
+    './src/blog.html',
+    './src/blog-detail.html',
+    './src/index.html',
+    '../layouts/**/*.html'
+  ],
+  theme: {
+    screens: {
+      ...defaultTheme.screens
+    },
+    colors: colors,
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      textShadow: {
+        sm: '0 1px 2px black',
+        DEFAULT: '0 2px 4px black',
+        lg: '0 8px 16px black',
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+          {
+            'text-shadow': (value) => ({
+              textShadow: value,
+            }),
+          },
+          { values: theme('textShadow') }
+      )
+    }),
+  ],
+}
